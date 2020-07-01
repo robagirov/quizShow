5c648c42e8fd8e1654be19ce7e5f3dc5fcbeb293
Rovshan Bagirov <bgrv.rvshn@gmail.com>
2020-07-01 18:19:07 +0300

    completed

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Backdrop.module.scss";
+
+const Backdrop = (props) => (
+    <div className={classes.Backdrop} onClick={props.onClick}></div>
+);
+
+export default Backdrop;
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import classes from "./Quiz.module.scss";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
+import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
+
+class Quiz extends Component {
+    state = {
+        results: {}, // {[id]: success error}
+        isFinished: false,
+        activeQuestion: 0,
+        answerState: null, // {[id]: 'success' 'error'}
+        quiz: [
+            {
+                question: "Какого цвета небо?",
+                id: 1,
+                answers: [
+                    {
+                        text: "Черный",
+                        id: 1,
+                    },
+                    {
+                        text: "Синий",
+                        id: 2,
+                    },
+                    {
+                        text: "Красный",
+                        id: 3,
+                    },
+                    {
+                        text: "Зеленый",
+                        id: 4,
+                    },
+                ],
+                rightAnswerId: 2,
+            },
+            {
+                question: "В каком году основали Санкт-Петербург?",
+                id: 2,
+                answers: [
+                    {
+                        text: "1700",
+                        id: 1,
+                    },
+                    {
+                        text: "1702",
+                        id: 2,
+                    },
+                    {
+                        text: "1703",
+                        id: 3,
+                    },
+                    {
+                        text: "1804",
+                        id: 4,
+                    },
+                ],
+                rightAnswerId: 3,
+            },
+        ],
+    };
+
+    onAnswerClickHandler = (answerId) => {
+        if (this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0];
+            if (this.state.answerState[key] === "success") {
+                return;
+            }
+        }
+
+        const question = this.state.quiz[this.state.activeQuestion];
+        const results = this.state.results;
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = "success";
+            }
+
+            this.setState({
+                answerState: { [answerId]: "success" },
+                results,
+            });
+
+            const timeout = window.setTimeout(() => {
+                if (this.isQuizFinished()) {
+                    this.setState({
+                        isFinished: true,
+                    });
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null,
+                    });
+                }
+                window.clearTimeout(timeout);
+            }, 1000);
+        } else {
+            results[question.id] = "error";
+            this.setState({
+                answerState: { [answerId]: "error" },
+                results,
+            });
+        }
+    };
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length;
+    }
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {},
+        });
+    };
+
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+
+                    {this.state.isFinished ? (
+                        <FinishedQuiz
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                        />
+                    ) : (
+                        <ActiveQuiz
+                            answers={
+                                this.state.quiz[this.state.activeQuestion]
+                                    .answers
+                            }
+                            question={
+                                this.state.quiz[this.state.activeQuestion]
+                                    .question
+                            }
+                            onAnswerClick={this.onAnswerClickHandler}
+                            quizLength={this.state.quiz.length}
+                            answerNumber={this.state.activeQuestion + 1}
+                            state={this.state.answerState}
+                        />
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quiz;
